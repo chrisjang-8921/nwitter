cdacfac5bfb15518c7af88ec2c32b465f827deb4
chrisjang-8921 <57851488+chrisjang-8921@users.noreply.github.com>
2020-10-05 21:24:00 +0900

    log in

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,79 @@
-import React from "react";
-const Auth = () => <span>Auth</span>;
+import { authService } from "fbase";
+import React, { useState } from "react";
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create new account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev); //new account의 이전값을 가지고 와서 그 값의 반대를 리턴
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placehold="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placehold="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Log in" : "Create Account"}
+      </span>
+      <div>
+        <button>Continue With Google</button>
+        <button>Continue With GitHub</button>
+      </div>
+    </div>
+  );
+};
+
 export default Auth;
